943f64d533d01a5d187167ce95a3fc20e16325ce
1stWorldMaster <pranaythapak2004@gmail.com>
2024-07-18 14:25:33 +0530

    Installation script for arch

---
+++ install.sh
@@ -0,0 +1,167 @@
+echo "Lets view the disk file"
+lsblk #command to view the file 
+echo "Press Enter to continue..."
+read
+
+# Function to prompt for device name
+get_device_name() {
+  while true; do
+    echo "Please enter the device name (e.g., /dev/sda):"
+    read device_name
+
+    # Check if the device name is not empty
+    if [ -n "$device_name" ]; then
+      break
+    else
+      echo "No device name entered. Please try again."
+    fi
+  done
+}
+
+# Function call to get the device name to continue the installation
+get_device_name
+
+# Use the entered device name with cfdisk
+cfdisk "$device_name"
+
+# Unset the device_name variable to release the memory
+unset device_name
+
+clear
+
+#Again give the instructions
+lsblk
+
+
+# Function to prompt for device name
+get_device_name() {
+  local prompt_message=$1
+  local device_name
+
+  while true; do
+    echo "$prompt_message"
+    read device_name
+
+    # Check if the device name is not empty
+    if [ -n "$device_name" ]; then
+      echo $device_name
+      return
+    else
+      echo "No device name entered. Please try again."
+    fi
+  done
+}
+
+# Get the device names
+efi_disk=$(get_device_name "Please enter the device name for the EFI partition (e.g., /dev/sda1):")
+main_disk=$(get_device_name "Please enter the device name for the main partition (e.g., /dev/sda2):")
+swap_disk=$(get_device_name "Please enter the device name for the swap partition (e.g., /dev/sda3):")
+
+# Format the disks
+mkfs.fat -F32 "$efi_disk"
+mkfs.ext4 "$main_disk"
+mkswap "$swap_disk"
+
+clear
+lsblk
+
+mount "$main_disk" /mnt 
+mkdir /mnt/boot
+mount "$efi_disk" /mnt/boot
+swapon "$swap_disk"
+
+clear
+lsblk
+
+pacstrap -i /mnt base base-devel linux linux-firmware sudo neofetch htop amd-ucode nano vim bluez bluez-utils networkmanager
+
+clear
+lsblk
+
+genfstab -U /mnt >> /mnt/etc/fstab 
+cat /mnt/etc/fstab
+
+#Timer Function
+echo "Starting 5-second timer..."
+sleep 5
+echo "Timer expired!"
+
+clear
+
+arch-chroot /mnt
+
+echo "Testing"
+
+neofetch
+
+#Timer Function
+echo "Starting 5-second timer..."
+sleep 5
+echo "Timer expired!"
+
+clear
+
+echo "Type the password for sudo account"
+passwd
+
+echo "Type the user name "
+read user_name
+
+useradd -m -g users -G wheel, storage,power,video,audio -s /bin/bash "$user_name"
+
+echo "Type the user passwd"
+passwd "$user_name"
+
+echo "Uncomment the line with wheel may be 3rd last line"
+
+#Timer Function
+echo "Starting 5-second timer..."
+sleep 5
+echo "Timer expired!"
+
+EDITOR= nano visudo
+
+su "$user_name"
+
+clear
+
+sudo pacman -Syu
+
+exit
+
+ln -sf /ur/share/zoneinfo/Asia/Kolkata /etc/localtime
+hwclock --systohc
+
+#Post installation
+
+
+# Localization
+echo "en_US.UTF-8 UTF-8" > /etc/locale.gen
+locale-gen
+echo "LANG=en_US.UTF-8" > /etc/locale.conf
+echo "KEYMAP=us" > /etc/vconsole.conf
+
+# Network configuration
+echo "myhostname" > /etc/archlinux
+echo "127.0.0.1 localhost" > /etc/hosts
+echo "::1       localhost" >> /etc/hosts
+echo "127.0.1.1 myhostname.localdomain archlinux" >> /etc/hosts
+
+
+# Install and configure GRUB
+pacman -S --noconfirm grub efibootmgr dosfstools mtools
+lsblk
+grub-install --target=x86_64-efi --efi-directory=/boot --bootloader-id-GRUB
+grub-mkconfig -o /boot/grub/grub.cfg
+
+# Enable essential services
+systemctl enable bluetooth
+systemctl enable networkmanager
+
+exit 
+
+unmount -lR /mnt
+
+echo "Post-installation complete. Exit chroot and reboot."
+
+shutdown now
