3129f47d3d9791135e717488384ddf29c0e8f541
1stWorldMaster <pranaythapakoffical@gmail.com>
2025-02-11 16:22:56 +0530

    updated

---
+++ file_handle/extras/python_script.sh
@@ -0,0 +1,5 @@
+virutal venv
+source venv/bin/activate
+code --enable-proposed-api ms-toolsai.jupyter
+
+python3 -m ipykernel install --name "new_venv" --user
--- file_handle/extras/script.sh
+++ file_handle/extras/script.sh
@@ -1,5 +1,42 @@
-virutal venv
-source venv/bin/activate
-code --enable-proposed-api ms-toolsai.jupyter
+#!/bin/bash
+countdown() {
+    local seconds=$1
+    while [ $seconds -gt 0 ]; do
+        echo -ne "Time remaining: $seconds\033[0K\r"
+        sleep 1
+        : $((seconds--))
+    done
+    echo -e "\nTime's up!"
+}
 
-python3 -m ipykernel install --name "new_venv" --user
+home(){
+	cd ~/projects/blender-git/
+}
+
+# Setting up the timer 
+start_time=$(date +%s)
+
+# Installing the git Updates
+notify-send "Blender" "Enter your password in window 1"
+sudo pacman -S --noconfirm base-devel subversion cmake libepoxy libxi libxcursor libxrandr libxinerama libxxf86vm python wayland-protocols libxkbcommon libdecor
+home
+notify-send "Blender" "Starting the download from the git repo and other dependencies"
+git clone --depth 1 https://projects.blender.org/blender/blender.git
+cd blender
+
+# Downloading libraries
+notify-send "Blender" "Starting with loding external libs"
+echo "Waiting for checking of something"
+./build_files/utils/make_update.py --use-linux-libraries
+
+notify-send "Blender" "Building blender has started"
+echo "Check the updates"
+make update
+notify-send "Blender" "Checking and making the files"
+make
+
+end_time=$(date +%s)
+runtime=$((end_time - start_time))
+echo "The script ran for $runtime seconds."
+notify-send "Blender" "The program is build in $runtime"
+
